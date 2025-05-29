752f5592f3957055195d631349168d45ea996897
ahmad562 <ahmadwaqas562@gmail.com>
2025-05-29 15:48:29 +0500

    working with fs module create multi ifiles and list dir files

--- showFile.js
+++ showFile.js
@@ -6,4 +6,9 @@ const myFolder = path.join(__dirname,'myfolder');
 for(let i = 0; i<3; i++){
     fs.writeFileSync(`${myFolder}/show${i}.txt`, "this is file no:"+i);
 }
-fs.readdir(myFolder)
+fs.readdir(myFolder, (err, items)=>{
+    //console.log(items);
+    items.forEach((file)=>{
+        console.log(file)
+    })
+})
