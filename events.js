c4ca1294ad3932167540119865ad081d745f59a5
ahmad562 <ahmadwaqas562@gmail.com>
2025-05-30 10:36:26 +0500

    BUilt in Events and Custom events in Nodejs

---
+++ events.js
@@ -0,0 +1,21 @@
+// built in events in nodejs
+
+//const fs = require('fs');
+const events = require('events');
+// read file
+// const myread = fs.createReadStream("./codeModules.txt");
+
+// myread.on('open',()=>{
+//     console.log("File opened successfully");
+// });
+
+
+// custome events
+
+const eventEmitter = new events.EventEmitter();
+
+eventEmitter.on('play', (type)=>{
+    console.log("i am playing "+type);
+})
+
+eventEmitter.emit('play','cricket');
