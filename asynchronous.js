45578dd4ab179756ba782cfeb0585551326ec661
ahmad562 <ahmadwaqas562@gmail.com>
2025-05-30 09:51:48 +0500

    promisis with .then and asyn await

--- asynchronous.js
+++ asynchronous.js
@@ -1,9 +1,36 @@
-const firstname = "Waqas";
+// const firstname = "Waqas";
+// let lastname = '';
+
+// setTimeout(()=>{
+//     lastname = "Ahmad";
+//     console.log("lastname");
+// },2000);
+
+// console.log(firstname + " " + lastname);
+
+// promises
+let fisrtname = "Waqas";
 let lastname = '';
 
-setTimeout(()=>{
-    lastname = "Ahmad";
-    console.log("lastname");
-},2000);
+const fullname = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        //lastname = "Ahmad";
+        resolve("Ahmad");
+    },10000);
+});
 
-console.log(firstname + " " + lastname);
+// .then
+// let newfullname = fullname.then((data)=>{
+//     lastname = data;
+//     console.log(`${fisrtname} ${lastname}`);
+// });
+
+
+// async await
+
+let display_full_name = async () => {
+    lastname = await fullname;  // fullname is the promis name
+    console.log(`${fisrtname} ${lastname}`);
+}
+
+display_full_name();
