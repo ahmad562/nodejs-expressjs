4fc288862af11f61433f59e81882f539a0b026eb
ahmad562 <ahmadwaqas562@gmail.com>
2025-05-30 09:23:11 +0500

    Asynchronous vs synchronous

---
+++ asynchronous.js
@@ -0,0 +1,9 @@
+const firstname = "Waqas";
+let lastname = '';
+
+setTimeout(()=>{
+    lastname = "Ahmad";
+    console.log("lastname");
+},2000);
+
+console.log(firstname + " " + lastname);
---
+++ crud_fs.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+const myCRUD = path.join(__dirname, "CRUD");
+const myPath  = `${myCRUD}/crud.txt`;
+
+// create
+//fs.writeFileSync(myPath, "Create Read Update and Delete Operation");
+
+// read
+fs.readFile(myPath, 'utf8', (error, file) => {
+   // console.log(file.toString());
+    console.log(file);
+})
+
+//update 
+fs.appendFile(myPath, " in node js file system", (error) => {
+    if(!error){
+        console.log("File Updated Successfully");
+    }
+})
+
+// delete file
+fs.unlinkSync(myPath);
+
+//rename file
+const newpath = `${myCRUD}/newnameCRUD.txt`;
+fs.rename(myPath, newpath ,(error) => {
+    if(!error){
+        console.log("File renamed successfully");
+    }
+});
